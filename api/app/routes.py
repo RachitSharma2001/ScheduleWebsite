d0e839d4d52a8bdeaff2bd2187d7e98af02585eb
RachitSharma2001 <rachitsharma613@gmail.com>
2021-09-26 16:13:00 -0700

    Running into errors communicating with front end and backend

--- api/app/routes.py
+++ api/app/routes.py
@@ -1,7 +1,20 @@
+import time
 from app import app
-
-@app.route('/')
+'''
+@app.route('/getmessage')
 def homepage():
-    return "This is backend"
+    print("We came to the backend!")
+    return {"message": "From backend"
+    
+    }
+    import time
+from flask import Flask
+
+app = Flask(__name__)'''
+
+@app.route('/time')
+def get_current_time():
+    return "Hey from backend"
+    #return {'time': time.time()}
 
 
--- src/App.js
+++ src/App.js
@@ -1,7 +1,19 @@
+import React, { useState, useEffect } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  const [currentTime, setCurrentTime] = useState(0);
+
+  useEffect(() => {
+    /*fetch('/time').then(res => res.json()).then(data => {
+      setCurrentTime(data.time);
+    });*/
+    fetch('http://127.0.0.1:5000/time').then(res => {
+      console.log(res.url);
+    });
+  }, []);
+
   return (
     <div className="App">
       <header className="App-header">
@@ -17,9 +29,47 @@ function App() {
         >
           Learn React
         </a>
+        <p>The current time is {currentTime}.</p>
       </header>
     </div>
   );
 }
 
 export default App;
+
+/*import React, { useState, useEffect } from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  var [message, setMessage] = useState(0);
+
+  useEffect(() => {
+    fetch('/getmessage').then(res => res.json()).then(data => {
+      console.log("Message: " + data.message);
+      setMessage(data.message);
+    });
+  }, []);
+
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      <p> Message: {message} </p>
+      </header>
+    </div>
+  );
+}
+
+export default App;*/
