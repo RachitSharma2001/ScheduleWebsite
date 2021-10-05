b719d80b1a0dc029c8b8fcd2d5de521eecda8486
RachitSharma2001 <rachitsharma613@gmail.com>
2021-10-05 12:13:13 -0700

    Added a button to the popup that adds the list of todos to the view.

--- src/App.js
+++ src/App.js
@@ -7,25 +7,8 @@ import './App.css';
 import './Popup.css';
 import './TodoBox.css';
 
-function App() {
-  const [addTodo, setAddTodo] = useState(false);
-  const [addEntry, setAddEntry] = useState(false);
-  // List of entries of the todo in the current popup windowd
-  const [entryList, setEntryList] = useState([]);
-  // List of entries for each todo
-  const [todoList, setTodoList] = useState([]);
-
-  const toggleAddTodo = () => {
-    // If the todo button was clicked, create a new todo database object
-    if(!addTodo){
-      fetch("http://localhost:5000/addTodo", {method: "POST"}).then(res => res.json()).then(data => {
-      });
-    }
-    setAddTodo(!addTodo);
-  };
-
-  useEffect(() => {
-    fetch("http://localhost:5000/getTodos").then(res => res.json()).then(data => {
+function updateTodos(todoList, setTodoList){
+  fetch("http://localhost:5000/getTodos").then(res => res.json()).then(data => {
       // Add the todos with > 0 entries
       let tempTodoList = [];
       for(let i = 0; i < data.todoList.length; i++){
@@ -38,8 +21,30 @@ function App() {
       }
       
       setTodoList(todoList => tempTodoList);
-    });
+  });
+}
+
+function App() {
+  const [addTodo, setAddTodo] = useState(false);
+  const [addEntry, setAddEntry] = useState(false);
+  // List of entries of the todo in the current popup windowd
+  const [entryList, setEntryList] = useState([]);
+  // List of entries for each todo
+  const [todoList, setTodoList] = useState([]);
+  
+  // Display the current todos right when user loads window
+  useEffect(() => {
+    updateTodos(todoList, setTodoList);
   }, []);
+
+  const toggleAddTodo = () => {
+    // If the todo button was clicked, create a new todo database object
+    if(!addTodo){
+      fetch("http://localhost:5000/addTodo", {method: "POST"}).then(res => res.json()).then(data => {
+      });
+    }
+    setAddTodo(!addTodo);
+  };
 
   const updateEntryList = (entryText) => {
     console.log("Entry list length: " + entryList.length);
@@ -49,17 +54,25 @@ function App() {
   const toggleAddEntry = () => {
     setAddEntry(!addEntry);
   };
+
+  const todoAdded = () => {
+    // Close popup
+    toggleAddTodo();
+    // Call function to update the todos
+    updateTodos(todoList, setTodoList);
+  };
   
   return (
     <div className="App">
       <header className="App-header">
-        <Button id="TodoAdd" buttonLabel="Add Todo for a day" height="200px" width="200px" onClick={toggleAddTodo}></Button>
+        <Button id="TodoAdd" buttonLabel="Add Todo for a day" height="200px" width="200px" onClick={toggleAddTodo}/>
         
         {todoList.map((todos) => <div className="todoBordBox"> {todos} </div>)}
         <div className="todoBordBox"> <p> Stuff </p> <p> Other Stuff </p> </div>
         {addTodo && <Popup content={<>
           <ul> {entryList.map((entry) => <li key = {entry.id}> {entry.text} </li>)} </ul>
           <EntryForm url="http://localhost:5000/addEntry/" todoId="2" submitCallBack={updateEntryList}/>
+          <Button id="finishedAdding" buttonLabel="Done" height="200px" width="200px" onClick={todoAdded}/>
         </>} handleClose={toggleAddTodo}></Popup>}
       </header>
     </div>
