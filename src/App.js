36e7ff7d7a724dfbc23d66463e7cd599755a6810
RachitSharma2001 <rachitsharma613@gmail.com>
2021-10-21 14:07:02 -0700

    Added comments and formatted code nicely

--- src/App.js
+++ src/App.js
@@ -4,26 +4,30 @@ import PublicNav from './PublicNav.jsx';
 import Home from './Home.jsx';
 import SignUp from './CreateAccount.jsx';
 import LogIn from './LogIn.jsx';
-//import useToken from './useToken';
 
+// Get the token that is stored in browser history
 const getToken = () => {
   const tokenString = localStorage.getItem('token');
   const userToken = JSON.parse(tokenString);
   return userToken;
 };
 
+// Function called when user signs in successfully
 const saveToken = (userToken, setToken) => {
-  console.log("Set token is called: " + userToken);
+  // Store user information in browser
   localStorage.setItem('token', JSON.stringify(userToken));
+  // Trigger react hook so page reloads to user home screen
   setToken(userToken);
 };
 
 function App(){
+  // Set token saved in browser as react hook
   const [token, setToken] = useState(getToken());
+  // Server url
   let backEndUrl = "http://localhost:5000/api";
 
+  // If the token does not exist, prompt the user to sign in
   if(!token){
-    //console.log("Token seems undefined");
     return (<div className="PublicView"> 
       <Router> 
         <PublicNav/> 
@@ -32,7 +36,8 @@ function App(){
       </Router>
     </div>);
   }
-  //console.log("Returning todolist instead");
+
+  // If the token exists, show the home screen of the signed in user
   return (
     <div className="App">
       <Router>
